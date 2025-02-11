3d92742f02123a990d71e5cb805e1422bb73fc02
jingu0318 <18dlwlsrn@gmail.com>
2025-02-11 17:58:14 +0900

    상품 컴포너트화 및 데이터바인딩

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     <div>
     <Container>
       <Row>
-        <Col className='col'>
+        {/* <Col className='col'>
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"></img>
           <h4>{shoes[0].title}</h4>
           <p>{shoes[0].price}</p>
@@ -38,13 +38,25 @@ function App() {
         <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"></img>
           <h4>{shoes[2].title}</h4>
           <p>{shoes[2].price}</p>
-        </Col>
+        </Col> */}
+        <Item shoes = {shoes}></Item>
       </Row>
     </Container>
     </div>
 
     </div>
   )
+}
+
+function Item (props) {
+  return(
+        <Col className='col'>
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"></img>
+          <h4>{props.shoes[0].title}</h4>
+          <p>{props.shoes[0].price}</p>
+        </Col>
+  )
+  
 }
 
 export default App
