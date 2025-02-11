86e0d4730133e2b19acd67d6ad9014425402a074
jingu0318 <18dlwlsrn@gmail.com>
2025-02-11 18:37:53 +0900

    불필요한 요소 제거

--- src/App.jsx
+++ src/App.jsx
@@ -24,21 +24,6 @@ function App() {
     <div>
     <Container>
       <Row>
-        {/* <Col className='col'>
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"></img>
-          <h4>{shoes[0].title}</h4>
-          <p>{shoes[0].price}</p>
-        </Col>
-        <Col>
-        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"></img>
-          <h4>{shoes[1].title}</h4>
-          <p>{shoes[1].price}</p>
-        </Col>
-        <Col>
-        <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"></img>
-          <h4>{shoes[2].title}</h4>
-          <p>{shoes[2].price}</p>
-        </Col> */}
         <Item shoes={shoes}></Item>
       </Row>
     </Container>
