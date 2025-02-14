6ccd1beb056ce2d6aaad7164adc0fed70e685e6c
jingu0318 <18dlwlsrn@gmail.com>
2025-02-14 19:36:25 +0900

    응용 :상세페이지 배열순으로 데이터바인딩이 아닌 특정 고유번호로 데이터바인딩

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <Navbar.Brand onClick={() => { navigate('/')}}>HaloSHOP</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link onClick={() => { navigate('/')}}>홈</Nav.Link>
-            <Nav.Link  onClick={() => { navigate('/detail')}}>상세페이지</Nav.Link>
+            <Nav.Link  onClick={() => { navigate('/detail/0')}}>상세페이지</Nav.Link>
             <Nav.Link href="#pricing">reshows</Nav.Link>
           </Nav>
         </Container>
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -4,21 +4,24 @@ function Detail (props) {
 
     let{id} = useParams();
     //let url = "https://codingapple1.github.io/shop/shoes"+(id+1)+".jpg"
+    let shoe = props.shoes.find((a)=>{
+        return a.id == id
+    })
 
-    return(
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-                </div>
-                <div className="col">
-                <h4 className="pt-5">{props.shoes[id].title}</h4>
-                <p>{props.shoes[id].content}</p>
-                <p>{props.shoes[id].price}원</p>
-                <button className="btn btn-danger">주문하기</button> 
+    return( 
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                    </div>
+                    <div className="col">
+                    <h4 className="pt-5">{shoe.title}</h4>
+                    <p>{shoe.content}</p>
+                    <p>{shoe.price}원</p>
+                    <button className="btn btn-danger">주문하기</button> 
+                    </div>
                 </div>
             </div>
-        </div> 
     )
 }
 
