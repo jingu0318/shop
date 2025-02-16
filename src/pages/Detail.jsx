c71d0e7fcf70eb8230edcbdba77c731a74b17e9b
jingu0318 <18dlwlsrn@gmail.com>
2025-02-16 21:22:19 +0900

    useEffect를 이용한 재랜더링 후 특정시간 이후에 동작하는 UI 만들기(동적인 UI 만드는 방법)

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,19 +1,24 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 
 function Detail (props) {
 
     let{id} = useParams();
+    let [discount, setdiscount] = useState(true);
     //let url = "https://codingapple1.github.io/shop/shoes"+(id+1)+".jpg"
-    let shoe = props.shoes.find((a)=>{
-        return a.id == id
-    })
+    let shoe = props.shoes.find((a) => a.id == id)
+
+
+    useEffect(()=>{
+        //여기적은 코드는 컴포넌트 로드 & 업데이트 마다 실행됨
+        setTimeout(() => {
+            setdiscount(false)
+        }, 2000);
+      });
 
     return( 
             <div className="container">
-                <Box>
-                <Yellowbtn bg="blue">버튼</Yellowbtn>
-                </Box>
+                {discount == true ? <Discount/> : null }
                 <div className="row">
                     <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
@@ -26,6 +31,14 @@ function Detail (props) {
                     </div>
                 </div>
             </div>
+    )
+}
+
+function Discount() {
+    return(
+        <div className="alert alert-warning">
+            2초 안에 구매시 할인
+        </div>
     )
 }
 
