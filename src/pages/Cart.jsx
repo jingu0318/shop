f823e0960ed5b8faae83c56e531a35f853a5b76d
jingu0318 <18dlwlsrn@gmail.com>
2025-02-24 20:05:49 +0900

    zustand와 redux 구분

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,24 @@
 import {Table} from 'react-bootstrap';
 import { useDispatch, useSelector } from "react-redux"
 import { plusNum } from './../store/cartSlice.jsx'
+import { useBear } from './../store.jsx';
 
 function Cart() {
     let cart = useSelector((state) => state.cart )
     let user = useSelector((state) => state.user)
-    // let oneCart = cart.find((a)=>{
-    //     return a.id == 0
-    // })
     let dispatch = useDispatch()
+
+    //redux는 함수를 사용하기 위해 useDispatch() 함수를 불러와야되지만 zustand는 그럴 필요가 없다.
+    const {bear, increaseBear, removeAllBear} = useBear();
 
     return(
         <div>
             <h6>{user}의 장바구니</h6>
+            <div>
+                <h2>Count : {bear}</h2>
+                <button onClick= {increaseBear}>increase</button>
+                <button onClick= {removeAllBear}>reset</button>
+            </div>
             <Table>
                 <thead>
                     <tr>
--- src/store.jsx
+++ src/store.jsx
@@ -1,21 +1,32 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
-import cart from './store/cartSlice'
+import { create } from 'zustand';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import cart from './store/cartSlice'; // cartSlice가 있다고 가정
 
+// Zustand 상태 관리
+export const useBear = create((set) => ({
+  bear: 0,
+  increaseBear: () => set((state) => ({ bear: state.bear + 1 })),
+  removeAllBear: () => set({ bear: 0 }),
+}));
+
+// Redux 상태 관리
 let user = createSlice({
-  name : 'user',
-  initialState : 'kim',
-  reducers : {
-    changeName(state){
-      return 'john ' + state
-    }
-  }
-})
+  name: 'user',
+  initialState: 'kim',
+  reducers: {
+    changeName(state) {
+      return 'john ' + state;
+    },
+  },
+});
 
-export let { changeName } = user.actions 
+export let { changeName } = user.actions;
 
-export default configureStore({
-  reducer: { 
-    user : user.reducer,
-    cart : cart.reducer
-  }
-}) 
+const store = configureStore({
+  reducer: {
+    user: user.reducer,
+    cart: cart.reducer,
+  },
+});
+
+export default store; // ✅ Redux store를 default export
