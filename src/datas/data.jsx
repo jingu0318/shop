90606dd74cdd9c68dde0cba92d0197bb776898f9
jingu0318 <18dlwlsrn@gmail.com>
2025-03-07 17:24:29 +0900

    기본 데이터 추가

--- src/datas/data.jsx
+++ src/datas/data.jsx
@@ -18,6 +18,42 @@ let data = [
       title : "Grey Yordan",
       content : "Born in the States",
       price : 130000
+    },
+    {
+      id : 3,
+      title : "Flowey",
+      content : "only 5 inches",
+      price : 120000
+    },
+    {
+      id : 4,
+      title : "Baby shoes",
+      content : "for less than 6",
+      price : 120000
+    },
+    {
+      id : 5,
+      title : "Red Herring",
+      content : "Born in France",
+      price : 120000
+    },
+    {
+      id : 6,
+      title : "Sijang",
+      content : "from Dongmyo station",
+      price : 120000
+    },
+    {
+      id : 7,
+      title : "Tag gari",
+      content : "ali and taobao",
+      price : 120000
+    },
+    {
+      id : 8,
+      title : "Flyer",
+      content : "Refund not available",
+      price : 120000
     }
   ] 
 
